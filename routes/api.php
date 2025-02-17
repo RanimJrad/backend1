4521ae9763b784e3fe723cd0fd103b1da4fe8030
RanimJrad <Ranimjrad0@gmail.com>
2025-02-17 15:02:18 +0100

    back

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,5 @@ Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); 
+ 
